6017355ef467852e9b2fc9a26964cbb2a614dcaa
cristianorregodev <cristian.duquew@gmail.com>
2023-02-08 14:53:44 -0500

    fix(vistaProfile): Ajustes para responsive de la vista perfile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,50 +2,58 @@ import React from 'react'
 import imageProfile from '../assets/img/imageProfile.webp'
 import { IconText, IconImage, IconCode } from '../assets/icons'
 
-
-import { MdOutlineAccountCircle } from 'react-icons/md';
-import { AiOutlineWhatsApp } from 'react-icons/ai';
-import { HiOutlineMail, } from 'react-icons/hi';
+import { FaUser, FaRegEnvelope, FaWhatsapp } from 'react-icons/fa'
 export const Profile = () => {
-
-    return (
-        <section className='container__profile'>
-            <div className='container__header-profile'>
-                <h2>Hola,<strong> Nombre</strong>
-                </h2>
-                <div className='container__user'>
-                    <div>
-                        <img src={imageProfile} alt='imageProfile'></img>
-                    </div>
-                    <div>
-                        <h3>
-                            Perfil
-                        </h3>
-                        <p>Nombre</p>
-                    </div>
-                </div>
-                <div className='container__data'>
-                <p> <span><MdOutlineAccountCircle size={20} /></span> Nombre</p>
-                <p> <HiOutlineMail size={20} /> Correo</p>
-                <p> <AiOutlineWhatsApp size={20} /> Contacto</p>
+  return (
+    <section className="container__profile">
+      <div className="container wrapper">
+        <div className="container__header-profile">
+          <h2>
+            Hola,<strong> Nombre</strong>
+          </h2>
+          <div className="container__user">
+            <div>
+              <img src={imageProfile} alt="imageProfile"></img>
             </div>
+            <div className="profile__name">
+              <h3>Perfil</h3>
+              <p>Nombre</p>
             </div>
-            
+          </div>
+          <div className="container__data">
+            <p>
+              {' '}
+              <FaUser size={20} />
+              <span>Nombre</span>{' '}
+            </p>
+            <p>
+              {' '}
+              <FaRegEnvelope size={20} />
+              <span>Correo</span>
+            </p>
+            <p>
+              {' '}
+              <FaWhatsapp size={20} />
+              <span>Contacto</span>
+            </p>
+          </div>
+        </div>
 
-            <div className='container__buttons'>
-                <h4>Que deseas hacer</h4>
-                <a href="#">
-                    <img src={IconText} alt="Icon" />
-                </a>
-                <a href="#">
-                    <img src={IconImage} alt="Icon" />
-
-                </a>
-                <a href="#">
-                    <img className='code'src={IconCode} alt="Icon" />
-                </a>
-            </div>
-
-        </section>
-    )
+        <div className="container__buttons ">
+          <h4>Que deseas hacer</h4>
+          <div>
+            <a href="#">
+              <img src={IconText} alt="Icon" /> Texto
+            </a>
+            <a href="#">
+              <img src={IconImage} alt="Icon" /> Imágenes
+            </a>
+            <a href="#">
+              <img className="code" src={IconCode} alt="Icon" /> Código
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
