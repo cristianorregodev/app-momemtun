daea9a7eb63d493f7b10d5c4f63dce52c2e6b086
cristianorregodev <cristian.duquew@gmail.com>
2023-02-07 19:13:51 -0500

    feature(welcomeNavbar): Lista vista welcome y Layout de navegacion

---
+++ client/src/assets/icons/index.js
@@ -0,0 +1,19 @@
+import IconTextPurple from './text_icon_purple.svg'
+import IconImagePurple from './image_icon_purple.svg'
+import IconCodePurple from './code_icon_purple.svg'
+import AppLogo from './app_logo.svg'
+import TextIconTransparent from './text_icon_transparent.svg'
+import ImageIconTransparent from './image_icon_transparent.svg'
+import CodeIconTransparent from './code_icon_transparent.svg'
+import HomeIconTransparent from './home_icon_transparent.svg'
+
+export {
+  IconTextPurple,
+  IconImagePurple,
+  IconCodePurple,
+  AppLogo,
+  TextIconTransparent,
+  ImageIconTransparent,
+  CodeIconTransparent,
+  HomeIconTransparent,
+}
--- client/src/js/routes/App.jsx
+++ client/src/js/routes/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from '../../pages/Home'
-import { InicioSesion } from '../../pages/InicioSesion';
-import { Profile } from '../../pages/Profile';
+import { InicioSesion } from '../../pages/InicioSesion'
+import { Profile } from '../../pages/Profile'
+import { Welcome } from '../../pages/Welcome'
+import { Layout } from '../containers/Layout'
+
 export const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/login' element={<InicioSesion />} />
-        <Route path='/profile' element={<Profile />} />
-    </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<InicioSesion />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/welcome" element={<Welcome />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   )
 }
