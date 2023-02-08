457c7e2867ac45c3043d39a7dadcdc3dea90e0af
Hvmble <mmontoyaabadia@gmail.com>
2023-02-08 08:41:02 -0500

    Fix (Home - Login - _login _home ): Se corrige etiquetas  y nombre de componente

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,12 +5,11 @@ import { Link } from 'react-router-dom'
 
 export const Home = () => {
     return (
-        <div className='container__home '>
-            <header>
+        <section className='container__home'>
+            <div className='container__img'>
                 <img src={Logo} alt="logotipo" />
                 <h1>Momemtum<span>App</span></h1>
-            </header>
-            <main>
+            </div>
                 <div className='container__button'>
                     <Link to="/login">
                         <Button text={'Iniciar sesión'}></Button>
@@ -18,9 +17,7 @@ export const Home = () => {
                     <Button clase={true} text={'Registrarse'}></Button>
                 </div>
                 <a href="#"><p>Necesitas Ayuda?</p></a>
-
-            </main>
-        </div>
+        </section>
 
 
     )
