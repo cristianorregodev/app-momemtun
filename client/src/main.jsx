d09ef03edb1d720ab852f9e310dc41c0a4884ad2
cristianorregodev <cristian.duquew@gmail.com>
2023-02-17 14:05:57 -0500

    fix (main.jsx): Arregla import de app.scss

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from "./js/routes/App";
-import "./sass/App.scss";
-
+import { App } from './js/routes/App'
+import './sass/app.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
