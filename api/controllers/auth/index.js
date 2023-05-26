07d6f8a488bc860ec7b1dd7446d501200e2e382b
cristianorregodev <cristian.duquew@gmail.com>
2023-05-26 14:59:05 -0500

    Fix (Reset-password): Ajuste a configuracion de reestablecer contraseña

--- api/controllers/auth/index.js
+++ api/controllers/auth/index.js
@@ -4,38 +4,38 @@ const { User } = require('../../models')
 const { generateJWT } = require('../../helpers/generateJWT')
 
 const login = async (req = request, res = response) => {
-  const { email, password } = req.body
+    const { email, password } = req.body
+    try {
+        const user = await User.findOne({ email })
 
-  try {
-    const user = await User.findOne({ email })
+        //* Password validation
+        const isValidPassword = bcryptjs.compareSync(password, user.password)
 
-    //* Password validation
-    const isValidPassword = bcryptjs.compareSync(password, user.password)
-    if (!isValidPassword) {
-      return res.status(400).json({
-        message: 'La contraseña es incorrecta',
-      })
+        if (!isValidPassword) {
+            return res.status(400).json({
+                message: 'La contraseña es incorrecta',
+            })
+        }
+
+        //* JWT generation
+        const token = await generateJWT(user.id)
+
+        res.json({ user, token })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ result: false })
     }
-
-    //* JWT generation
-    const token = await generateJWT(user.id)
-
-    res.json({ user, token })
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ result: false })
-  }
 }
 
 const reset = async (req = request, res = response) => {
-  const { email } = req.body
-  try {
-    const userEmail = await User.findOne({ email })
-    res.json({ userEmail })
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ result: false })
-  }
+    const { email } = req.body
+    try {
+        const userEmail = await User.findOne({ email })
+        res.json({ userEmail })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ result: false })
+    }
 }
 
 module.exports = { login, reset }
--- api/controllers/users/index.js
+++ api/controllers/users/index.js
@@ -5,73 +5,75 @@ const { User } = require('../../models')
 const { generateJWT } = require('../../helpers/generateJWT')
 
 const usersGet = async (req = request, res = response) => {
-  const users = await User.find({ active: true })
+    const users = await User.find({ active: true })
 
-  res.json({ success: true, data: users })
+    res.json({ success: true, data: users })
 }
 
 const usersPost = async (req = request, res = response) => {
-  const { username, email, password, contact_number } = req.body
+    const { username, email, password, contact_number } = req.body
 
-  const user = new User({ username, email, password, contact_number })
+    const user = new User({ username, email, password, contact_number })
 
-  //* Hash password
-  const salt = bcryptjs.genSaltSync()
-  user.password = bcryptjs.hashSync(password, salt)
+    //* Hash password
+    const salt = bcryptjs.genSaltSync()
+    user.password = bcryptjs.hashSync(password, salt)
 
-  //* JWT generation
-  const token = await generateJWT(user.id)
+    //* JWT generation
+    const token = await generateJWT(user.id)
 
-  await user.save()
-  res.json({ user, token })
+    await user.save()
+    res.json({ user, token })
 }
 
 const usersPut = async (req = request, res = response) => {
-  const { id } = req.params
-  const { password, email, ...rest } = req.body
+    const { id } = req.params
+    const { password, email, ...rest } = req.body
 
-  if (password) {
-    //* Hash password
-    const salt = bcryptjs.genSaltSync()
-    rest.password = bcryptjs.hashSync(password, salt)
-  }
+    if (password) {
+        //* Hash password
+        const salt = bcryptjs.genSaltSync()
+        rest.password = bcryptjs.hashSync(password, salt)
+    }
 
-  await User.findByIdAndUpdate(id, rest)
-  const user = await User.findById(id)
+    await User.findByIdAndUpdate(id, rest)
+    const user = await User.findById(id)
 
-  res.json({ success: true, data: user })
+    res.json({ success: true, data: user })
 }
 
 const usersPatch = async (req = request, res = response) => {
-  const { id } = req.params
-  const { new_password } = req.body
+    const { id } = req.params
+    const { new_password } = req.body
 
-  //* Hash password
-  const salt = bcryptjs.genSaltSync()
-  const password = bcryptjs.hashSync(new_password, salt)
+    //* Hash password
+    const salt = bcryptjs.genSaltSync()
+    const password_hashed = bcryptjs.hashSync(new_password, salt)
+    const userFinded = await User.findById(id)
+    userFinded.password = password_hashed
 
-  await User.findByIdAndUpdate(id, password)
-  const user = await User.findById(id)
+    await User.findByIdAndUpdate(id, userFinded)
+    const user = await User.findById(id)
 
-  res.json({ success: true, data: user })
+    res.json({ success: true, data: user })
 }
 
 const usersDelete = async (req = request, res = response) => {
-  const { id } = req.params
+    const { id } = req.params
 
-  await User.findByIdAndUpdate(id, { active: false })
-  const user = await User.findById(id)
+    await User.findByIdAndUpdate(id, { active: false })
+    const user = await User.findById(id)
 
-  res.json({
-    success: true,
-    data: user,
-  })
+    res.json({
+        success: true,
+        data: user,
+    })
 }
 
 module.exports = {
-  usersGet,
-  usersPost,
-  usersPut,
-  usersPatch,
-  usersDelete,
+    usersGet,
+    usersPost,
+    usersPut,
+    usersPatch,
+    usersDelete,
 }
